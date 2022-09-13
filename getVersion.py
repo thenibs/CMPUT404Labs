9f9dc32eee2775e7c284823194c33cc80d3f5077
Nibras Alam <nibras@cs.ualberta.ca>
2022-09-12 18:17:52 -0600

    Step 11

--- getVersion.py
+++ getVersion.py
@@ -3,3 +3,5 @@ import requests
 print(requests.__version__)
 
 print(requests.get("http://www.google.com/"))
+
+print("\n" + requests.get("https://raw.githubusercontent.com/thenibs/CMPUT404Labs/main/getVersion.py").text)
